df44ecd3258fa220c5cadf67d0c0a8f98f1d5eb8
Andres Osorio <andres.osoriogr@gmail.com>
2020-07-13 17:09:19 +0000

    finished implementing queries.sql

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -11,20 +11,44 @@ from
 order by
     cpu_number, total_mem desc;
 
-/* Average used memory in percentage over 5 mins interval for each host. (used memory = total memory
- * - free memory). */
+/* function to round timestamp to the nearest 5-min interval */
+create function round5(ts timestamp) returns timestamp as 
+$$
+begin
+    return date_trunc('hour', ts) + date_part('minute', ts):: int / 5 * interval '5 min';
+end;
+$$
+    language plpgsql;
+
+/* Average used memory in percentage over 5 mins interval for each host; (used memory = total memory) */
 select 
-    id, hostname, timestamp, average * 100 / total_mem as avg_used_mem_perc
+    id, hostname, round5(u.timestamp) as roundedts, avg(total_mem - memory_free) * 100 / total_mem as avg_used_mem_perc
+from
+    host_info as i
+join
+    host_usage as u
+on 
+    i.id = u.host_id
+group by
+    id, roundedts
+order by
+    id, roundedts;
+
+/* Query to detect node failure; the cron job is supposed to insert a new entry to the host_usage
+ * table every minute when the server is healthy; we can assume that a server fails when it
+ * inserts less than three data points within 5-min interval. */
+select
+    host_id, ts, num_data_points
 from 
     (
         select
-            id, hostname, date_trunc('minute', u.timestamp) as timestamp, total_mem, memory_free, avg(total_mem - memory_free) over (partition by id) average
+            host_id, round5(timestamp) as ts, count(round5(timestamp)) as num_data_points
         from 
-            host_info as i
-        join
-            host_usage as u
-        on
-            i.id = u.host_id
-    ) tmp
-order by 
-    id;
+            host_usage
+        group by
+             host_id, ts
+        order by
+            host_id, ts
+    ) as tmp
+where
+    num_data_points < 3;
