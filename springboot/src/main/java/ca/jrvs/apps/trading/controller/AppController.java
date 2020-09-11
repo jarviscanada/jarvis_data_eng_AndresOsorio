126e37a4205ae8d7ff43962c50a57905c8977e23
Andres Osorio <andres.osoriogr@gmail.com>
2020-09-11 20:06:24 +0000

    springboot: dockerize trading app

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/AppController.java
@@ -0,0 +1,23 @@
+package ca.jrvs.apps.trading.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping("/")
+public class AppController {
+
+  @ApiOperation(value = "Show health", notes = "Show health of the app")
+  @ResponseBody
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping(path = "/health")
+  public String getHealth() {
+    return "I'm healthy and busy!";
+  }
+
+}
